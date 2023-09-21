c7e5bc99727f61ee94a361d0a93d07653b6ca0ca
JRodriguezS <josuesonro@gmail.com>
2023-09-20 23:05:39 -0600

    I updated the songs.json and the main.go
    In this updated i added it all the song that were left in previous commits. Also I add three different methods to search a list of songs from the server, by the first letter of the name, the number of words in the name and by its size in MB.

--- Server/src/main.go
+++ Server/src/main.go
@@ -7,6 +7,9 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
+
 	"github.com/rs/cors"
 
 	"github.com/gorilla/mux"
@@ -39,16 +42,18 @@ func main() {
 	// UpdateAddSong
 	r.HandleFunc("/addSong", AddSongJson).Methods("POST")
 	r.HandleFunc("/delete/{Name}", deleteSong).Methods("POST")
-
 	r.HandleFunc("/getBuffer/{Name}", getBuffer).Methods("GET")
 
-
+	//Funciones para buscar canciones
+	r.HandleFunc("/songs/firstLetter/{Letter}", getSongsByFirstLetter).Methods("GET")
+	r.HandleFunc("/songs/wordcount/{Count}", searchSongsByWordCount).Methods("GET")
+	r.HandleFunc("/songs/fileSizeRange/{minSize}/{maxSize}", searchSongsByFileSizeRange).Methods("GET")
 
 	// Crear un middleware CORS
-    corsMiddleware := cors.New(cors.Options{
-        AllowedOrigins: []string{"http://localhost:3000"}, // Cambia esto según la URL de tu cliente web
-        AllowedMethods: []string{"GET", "POST"}, // Los métodos permitidos
-    })
+	corsMiddleware := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:3000"}, // Cambia esto según la URL de tu cliente web
+		AllowedMethods: []string{"GET", "POST"},           // Los métodos permitidos
+	})
 	handler := corsMiddleware.Handler(r)
 
 	http.Handle("/", handler)
@@ -127,45 +132,45 @@ func addSong(w http.ResponseWriter, r *http.Request) {
 
 func getBuffer(w http.ResponseWriter, r *http.Request) {
 	// Obtener el nombre del archivo MP3 de los parámetros
-    params := mux.Vars(r)
-    songName := params["Name"]
+	params := mux.Vars(r)
+	songName := params["Name"]
 
 	// Construir la ruta completa al archivo MP3 utilizando la variable FilePath en la estructura Song
-    var filePath string
-    for _, song := range songs {
-        if song.Name == songName {
-            filePath = os.Getenv("SONGS_PATH") + "/" + song.FilePath
-            break
-        }
-    }
+	var filePath string
+	for _, song := range songs {
+		if song.Name == songName {
+			filePath = os.Getenv("SONGS_PATH") + "/" + song.FilePath
+			break
+		}
+	}
 	if filePath == "" {
-        http.Error(w, "Song not found", http.StatusNotFound)
-        return
-    }
+		http.Error(w, "Song not found", http.StatusNotFound)
+		return
+	}
 	// Leer el archivo MP3 en bytes
-    mp3Bytes, err := ioutil.ReadFile(filePath)
+	mp3Bytes, err := ioutil.ReadFile(filePath)
 	fmt.Println(filePath)
-    if err != nil {
-        http.Error(w, "Error reading MP3 file", http.StatusInternalServerError)
-        return
-    }
+	if err != nil {
+		http.Error(w, "Error reading MP3 file", http.StatusInternalServerError)
+		return
+	}
 	// Configurar las cabeceras de la respuesta HTTP
 	w.Header().Set("Content-Type", "audio/mpeg")
 	w.Header().Set("Content-Length", string(len(mp3Bytes)))
 
 	// Escribir los bytes del archivo MP3 en la respuesta HTTP
 	_, err = w.Write(mp3Bytes)
-    if err != nil {
-        http.Error(w, "Error writing MP3 data to response", http.StatusInternalServerError)
-        return
-    }
+	if err != nil {
+		http.Error(w, "Error writing MP3 data to response", http.StatusInternalServerError)
+		return
+	}
 
 }
 
 // UpdateAddSong
 func AddSongJson(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	
+
 	var newSong Song
 
 	err := json.NewDecoder(r.Body).Decode(&newSong)
@@ -173,7 +178,6 @@ func AddSongJson(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Invalid request body", http.StatusBadRequest)
 		return
 	}
-
 
 	// Check if a song with the same name already exists
 	for _, existingSong := range songs {
@@ -237,4 +241,105 @@ func deleteSong(w http.ResponseWriter, r *http.Request) {
 
 	// Si no se encontró la canción, devuelve un error 404
 	http.Error(w, "Song not found", http.StatusNotFound)
+}
+
+// Funcion para buscar canciones por la primera letra
+func getSongsByFirstLetter(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	firstLetter := params["Letter"]
+
+	var matchingSongs []Song
+
+	for _, song := range songs {
+		if strings.HasPrefix(strings.ToLower(song.Name), strings.ToLower(firstLetter)) {
+			matchingSongs = append(matchingSongs, song)
+		}
+	}
+
+	if len(matchingSongs) == 0 {
+		http.Error(w, "No songs found with the specified first letter", http.StatusNotFound)
+		return
+	}
+
+	json.NewEncoder(w).Encode(matchingSongs)
+}
+
+// Funcion para buscar canciones por numero de palabras
+func searchSongsByWordCount(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	countStr := params["Count"]
+
+	count, err := strconv.Atoi(countStr)
+	if err != nil {
+		http.Error(w, "Invalid count parameter", http.StatusBadRequest)
+		return
+	}
+
+	matchingSongs := []Song{}
+	for _, song := range songs {
+		// Divide el nombre de la canción en palabras
+		words := strings.Fields(song.Name)
+
+		// Comprueba si el número de palabras coincide con el valor especificado
+		if len(words) == count {
+			matchingSongs = append(matchingSongs, song)
+		}
+	}
+
+	if len(matchingSongs) == 0 {
+		http.Error(w, "No songs found with the specified word count", http.StatusNotFound)
+		return
+	}
+
+	json.NewEncoder(w).Encode(matchingSongs)
+}
+
+// Funcion para buscar canciones por tamaño de archivo en megabytes
+func searchSongsByFileSizeRange(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	minSizeMBStr := params["minSize"]
+	maxSizeMBStr := params["maxSize"]
+
+	minSizeMB, err := strconv.Atoi(minSizeMBStr)
+	if err != nil {
+		http.Error(w, "Invalid minSize parameter", http.StatusBadRequest)
+		return
+	}
+
+	maxSizeMB, err := strconv.Atoi(maxSizeMBStr)
+	if err != nil {
+		http.Error(w, "Invalid maxSize parameter", http.StatusBadRequest)
+		return
+	}
+
+	matchingSongs := []Song{}
+	for _, song := range songs {
+		// Obtén el nombre del archivo MP3 de la canción
+		songFilePath := os.Getenv("SONGS_PATH") + "/" + song.FilePath
+
+		// Lee el tamaño del archivo MP3
+		fileInfo, err := os.Stat(songFilePath)
+		if err != nil {
+			// Maneja el error si no se puede acceder al archivo
+			continue
+		}
+
+		// Convierte el tamaño del archivo a MB
+		fileSizeMB := float64(fileInfo.Size()) / (1024 * 1024) // 1 MB = 1024 * 1024 bytes
+
+		// Compara el tamaño del archivo con el rango especificado en MB
+		if fileSizeMB >= float64(minSizeMB) && fileSizeMB <= float64(maxSizeMB) {
+			matchingSongs = append(matchingSongs, song)
+		}
+	}
+
+	if len(matchingSongs) == 0 {
+		http.Error(w, "No songs found within the specified file size range", http.StatusNotFound)
+		return
+	}
+
+	json.NewEncoder(w).Encode(matchingSongs)
 }
